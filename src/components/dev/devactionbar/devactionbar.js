a8a9c98b68be44753dc4b34580fe9219b463cfe8
Mike B. K <mikekristiansen88@gmail.com>
2024-01-10 11:36:41 +0100

    opg 7 done

--- src/components/dev/devactionbar/devactionbar.js
+++ src/components/dev/devactionbar/devactionbar.js
@@ -1,27 +1,63 @@
 'use client'
 import styles from './devactionbar.module.css'
 import React, { useState } from 'react'
+import { useEffect, useRef } from 'react'
 
-const DevActionBar = ({ setSizeFunction }) => {
+const DevActionBar = ({ setSizeFunction, size, config, onColorChange }) => {
+  const activeSlideRef = useRef(null)
+
+  // Function to handle color change
+  const handleColorChange = (e) => {
+    onColorChange(e.target.value) // Pass the selected color to the parent component
+  }
+
+  useEffect(() => {
+    let slider = activeSlideRef.current
+    slider.value = size
+  }, [size])
+
   return (
-    <div className={styles.actionBar}>
-      <span className={styles.btn} onClick={() => setSizeFunction(20)}>
-        Lille
-      </span>
-      <span className={styles.btn} onClick={() => setSizeFunction(50)}>
-        Mellem
-      </span>
-      <span className={styles.btn} onClick={() => setSizeFunction(100)}>
-        Stor
-      </span>
-      <span className={styles.btn}>
-        <input
-          type='range'
-          min='50'
-          max='150'
-          onChange={(e) => setSizeFunction(e.target.value)}
-        ></input>
-      </span>
+    <div className={styles.container}>
+      <div className={styles.status}>
+        <h1>{size}</h1>
+      </div>
+      <div className={styles.actionBar}>
+        <span
+          className={styles.btn}
+          onClick={() => setSizeFunction(config.small)}
+        >
+          {config.small}
+        </span>
+        <span
+          className={styles.btn}
+          onClick={() => setSizeFunction(config.medium)}
+        >
+          {config.medium}
+        </span>
+        <span
+          className={styles.btn}
+          onClick={() => setSizeFunction(config.large)}
+        >
+          {config.large}
+        </span>
+        <span className={styles.btn}>
+          <input
+            type='range'
+            min='50'
+            max='150'
+            onChange={(e) => setSizeFunction(e.target.value)}
+            defaultValue={size}
+            ref={activeSlideRef}
+          ></input>
+        </span>
+        <span className={styles.btn}>
+          <input
+            type='color'
+            className={styles.color}
+            onChange={handleColorChange} // Call handleColorChange on color change
+          ></input>
+        </span>
+      </div>
     </div>
   )
 }
--- src/components/devicons/devicons.js
+++ src/components/devicons/devicons.js
@@ -16,13 +16,30 @@ import React, { useState } from 'react'
 import DevActionBar from '../dev/devactionbar/devactionbar'
 
 const DevIcons = () => {
-  const [size, setSize] = useState(300)
+  const [size, setSize] = useState(150)
+  const [selectedColor, setSelectedColor] = useState('#000000') // State to store the selected color
 
-  let style = { width: `${size}px`, height: `${size}px` }
+  let style = { width: `${size}px`, height: `${size}px`, color: selectedColor }
+
+  let config = {
+    small: 50,
+    medium: 90,
+    large: 150,
+  }
+
+  // Function to handle color change
+  const handleColorChange = (color) => {
+    setSelectedColor(color)
+  }
 
   return (
     <div className={styles.container}>
-      <DevActionBar setSizeFunction={setSize}></DevActionBar>
+      <DevActionBar
+        setSizeFunction={setSize}
+        size={size}
+        config={config}
+        onColorChange={handleColorChange} // Pass the function to handle color change
+      ></DevActionBar>
 
       <div className={styles.icons}>
         <FaAddressBook style={style} />
