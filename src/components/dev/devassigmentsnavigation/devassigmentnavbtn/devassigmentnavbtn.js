79e3d83f6f62c10693528f1f66c9232d8446db68
Mike B. K <mikekristiansen88@gmail.com>
2024-01-11 09:37:45 +0100

    route done

--- src/components/dev/devassigmentsnavigation/devassigmentnavbtn/devassigmentnavbtn.js
+++ src/components/dev/devassigmentsnavigation/devassigmentnavbtn/devassigmentnavbtn.js
@@ -1,10 +1,14 @@
+'use client'
 import Link from 'next/link'
 import styles from '../devassigmentnavbtn/devassigmentnavbtn.module.css'
 import { FaJsSquare } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
 
 const DevAssignmentsNavBtn = ({ link, title }) => {
+  const pathname = usePathname()
+  const active = pathname === link ? styles.active : ''
   return (
-    <div className={styles.navBtn}>
+    <div className={`${styles.navBtn} ${active}`}>
       <Link href={`${link}`}>
         <FaJsSquare /> {title}
       </Link>
